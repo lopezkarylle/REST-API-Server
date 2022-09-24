a64050f70c1a11970e425e012d27d3da89642de6
lopezkarylle <dr.lopezkarylle@gmail.com>
2022-09-24 11:55:20 +0800

    first commit

---
+++ database/migrations/2022_09_24_034006_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string("Title", 55);
+            $table->string("Description", 255);
+            $table->string("Currency", 255);
+            $table->decimal("Price", 12);
+            $table->string("Category", 255);
+            $table->string("Image", 255);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};

6b59e4db1307bd123ef2dc373420df90c822c390
lopezkarylle <dr.lopezkarylle@gmail.com>
2022-10-08 11:02:20 +0800

    Rest API - completed

--- routes/api.php
+++ routes/api.php
@@ -15,15 +15,13 @@ use App\Models\Product;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::get('products/categories',[ProductsController::class, 'allCategory']);
-Route::get('products', [ProductsController::class, 'allProducts']);
-Route::get('products/{product_id}', [ProductsController::class, 'singleProduct']);
-Route::get('products/search/{title}', [ProductsController::class, 'searchProduct']);
-Route::get('products/category/{category_name}', [ProductsController::class, 'categoryProduct']);
-Route::post('products/add', [ProductsController::class, 'addProducts']);
-Route::put('products/{product_id}', [ProductsController::class, 'updateProducts']);
-Route::delete('products/{id}', [ProductsController::class, 'deleteProduct']);
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::controller(ProductsController::class)->group(function() {
+    Route::get('products/categories', 'allCategory');
+    Route::get('products', 'allProducts');
+    Route::get('products/{product_id}', 'singleProduct');
+    Route::get('products/search/{title}', 'searchProduct');
+    Route::get('products/category/{category_name}', 'categoryProduct');
+    Route::post('products/add', 'addProducts');
+    Route::put('products/{product_id}', 'updateProducts');
+    Route::delete('products/{id}', 'deleteProduct');
 });
